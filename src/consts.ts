ecfaf4fac94c320967820b4644e8dde7c7821ab2
Arnas Kazlauskas <arnas.kazlauskas@refurbed.com>
2025-05-17 23:49:18 +0300

    feat: desc

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Arnas Blog';
+export const SITE_DESCRIPTION = 'Personal blog about web development, software engineering, and my journey as a developer';
