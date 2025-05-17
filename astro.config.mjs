4c5df88d23347ca3e353cbed817eb17cdcc4eccd
Arnas Kazlauskas <arnas.kazlauskas@refurbed.com>
2025-05-17 22:21:44 +0300

    init commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'http://arn.as',
+	integrations: [mdx(), sitemap()],
+});
